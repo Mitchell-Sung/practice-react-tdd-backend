6fbf718fbf46d188319abe584db097c12a105d06
Mitchell-Sung <mitchell.sung@gmail.com>
2021-08-31 02:26:58 -0600

    Set up product router

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
-import express from 'express';
-
-const PORT = 5000;
+const express = require('express');
 
 const app = express();
+
+app.set('port', process.env.PORT || 5000);
+// const PORT = 5000;
+
+const productRoute = require('./routes/project.route.js');
+
+app.use('/api/products', productRoute);
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
 	res.send('Hello React TDD');
 });
 
-app.listen(PORT);
-
-console.log(`Status :>> Running on port ${PORT}`);
+app.listen(app.get('port'), () => {
+	console.log(`## Status :>> Running on port ${app.get('port')}`);
+});
+// app.listen(PORT);
+// console.log(`## Status :>> Running on port ${PORT}`);
