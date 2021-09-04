96cbf15c1cf7079fdbb894a9c8560faf3226673e
Mitchell-Sung <mitchell.sung@gmail.com>
2021-09-04 01:29:48 -0600

    Integrated test for ID existence

--- test/integration/getProductById.int.test.js
+++ test/integration/getProductById.int.test.js
@@ -10,3 +10,10 @@ test('GET /api/products/:productId', async () => {
 	expect(response.body.name).toBe(findProductByIdData.name);
 	expect(response.body.description).toBe(findProductByIdData.description);
 });
+
+test('GET ID does not exist /api/products/:productId', async () => {
+	const response = await request(app).get(
+		'/api/products/6131bb593995565d2661c555'
+	);
+	expect(response.statusCode).toBe(404);
+});
